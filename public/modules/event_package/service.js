d170983341354a8bf24eadc52238f70237c500f4
Your Name <you@example.com>
2016-05-25 11:07:36 +0530

    update changes

--- public/modules/event_package/service.js
+++ public/modules/event_package/service.js
@@ -10,54 +10,6 @@ angular.module('alisthub').factory('venues', ['$q', '$timeout','communicationSer
       
   };
   
-  // url.saveEvent = function(jsondata,callback){
-  //      communicationService.resultViaPost(webservices.saveEvent,appConstants.authorizationKey,headerConstants.json,jsondata, function(res,req){
-		// 	callback(res.data);
-		// });
-      
-  // };
-////////////save package///////////////
-  //   url.savePackage = function(jsondata,callback){
-  //      communicationService.resultViaPost(webservices.savePackage,appConstants.authorizationKey,headerConstants.json,jsondata, function(res,req){
-  //     callback(res.data);
-  //   });
-      
-  // };
-  
-  // url.saverecurringEvent = function(jsondata,callback){
-   
-  //      communicationService.resultViaPost(webservices.saverecurringEvent,appConstants.authorizationKey,headerConstants.json,jsondata, function(res,req){
-		// 	callback(res.data);
-		// });
-      
-  // };
-  //saverecurringPackage///
-  //   url.saverecurringPackage = function(jsondata,callback){
-   
-  //      communicationService.resultViaPost(webservices.saverecurringPackage,appConstants.authorizationKey,headerConstants.json,jsondata, function(res,req){
-  //     callback(res.data);
-  //   });
-      
-  // };
-  
-  // url.savepriceleveldata = function(jsondata,callback){
-   
-  //      communicationService.resultViaPost(webservices.savepricelevel,appConstants.authorizationKey,headerConstants.json,jsondata, function(res,req){
-		// 	callback(res.data);
-		// });
-      
-  // };
-  
-  // url.getPricelevel = function(jsondata,callback){
-  //      communicationService.resultViaPost(webservices.getPricelevel,appConstants.authorizationKey,headerConstants.json,jsondata, function(res,req){
-		// 	callback(res.data);
-		// });
-      
-  // };
-
-
-  
-  
 return url;
 
 }]);
