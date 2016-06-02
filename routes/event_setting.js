ed6bf9b03713bc27f93a7474f81e898d90ef5df0
Rajat Goel <rajatg@smartdatainc.net>
2016-06-02 12:44:04 +0530

    date checks for assign discounts

--- routes/event_setting.js
+++ routes/event_setting.js
@@ -168,6 +168,15 @@ module.exports = function(app, express) {
     /////////////////////// Export CSV  ////////////////////////////////
     router.post('/getSelectedDiscount', discount_setting.getSelectedDiscount); 
 
+   /* View Event Price levels   */
+    
+    router.post('/getEventPriceLevels', discount_setting.getEventPriceLevels);
+
+    /* Assign discount coupons to events and price levels   */
+    
+    router.post('/saveFinalAssignmet', discount_setting.saveFinalAssignmet);
+
+
     /*** Module : Manage Bundle
      *   Get Seller Bundle 
      *
