3655cb2c02198d54677b873724060f26792a2e05
Your Name <you@example.com>
2016-07-13 12:04:08 +0530

    updates widgets

--- public/modules/downloads/controller.js
+++ public/modules/downloads/controller.js
@@ -1,4 +1,21 @@
 angular.module('alisthub').controller('downloadController', function($scope, $state, $localStorage, $injector, ngTableParams, $uibModal, $rootScope, $filter, $timeout, $sce, $location)
  {
-    //For Step 1
+    
+
+/*Open the different tabs of event setting page as per request */
+$scope.id1=1;
+$scope.id2=1;
+
+  $scope.openTab1 = function(id) {
+        if (id == 1) {
+        	 $scope.id1=2;
+        	 $scope.windowTab=true;
+        	  // $scope.windowTab=false;
+        	 $scope.windowTab = "fa-caret-up";
+        	  // $scope.windowTab = "fa-caret-down";
+
+
+
+		}
+}
 });
--- public/modules/widgets/eventController.js
+++ public/modules/widgets/eventController.js
@@ -1,12 +1,25 @@
 angular.module('alisthub').controller('widgetcontroller', function($scope, $state, $localStorage, $injector, ngTableParams, $uibModal, $rootScope, $filter, $timeout, $sce, $location) {
-    $scope.eventId="22381";
-    $scope.ticket_uri="http://tickets.alistixs.com/events/22381";
-    $scope.data={};
-    $scope.data.title="Upcoming Events";
-    $scope.title_text=function()
-   {
-     $scope.embed_code='<script type="text/javascript">var EventsWidgetDisplayPreferences = {seller_id: '+$scope.eventId+',uri: "'+$scope.ticket_uri+'",domain: "tickets.alistixs.com",title_text: "'+$scope.data.title+'"}</script><script id="EventsWidgetScript" type="text/javascript" src="https://tickets.alistixs.com/js/events_widget.js"></script>';
-  } 
- $scope.embed_code='<script type="text/javascript">var EventsWidgetDisplayPreferences = {seller_id: '+$scope.eventId+',uri: "'+$scope.ticket_uri+'",domain: "tickets.alistixs.com",title_text: "'+$scope.data.title+'"}</script><script id="EventsWidgetScript" type="text/javascript" src="https://tickets.alistixs.com/js/events_widget.js"></script>';
+    $scope.eventId = "22381";
+    $scope.ticket_uri = "http://tickets.alistixs.com/events/22381";
+    $scope.data = {};
+    $scope.data.title = "Upcoming Events";
+    $scope.data.max_number_of_events = 4;
+    $scope.data.group_series_events = true;
+    $scope.data.display_title_bar = true;
+    $scope.data.display_date_block = true;
+    $scope.data.display_full_date = true;
+    $scope.data.display_time = true;
+    $scope.data.width = "330px";
+    $scope.data.height = "auto";
+    $scope.data.background_color = "#2c344b";
+    $scope.data.header_color = "#FFFFFF";
+    $scope.data.row_background_color = "#FFFFFF";
+    $scope.data.text_color = "#333";
+    $scope.data.link_color = "#fe1e5a";
+    console.log($scope.data);
+    $scope.title_text = function() {
+        $scope.embed_code = '<script type="text/javascript">var EventsWidgetDisplayPreferences = {seller_id: ' + $scope.eventId + ',uri: "' + $scope.ticket_uri + '",domain: "tickets.alistixs.com",  white_label: "ALIST Solutions LLC",title_text: "' + $scope.data.title + '",height: "' + $scope.data.height + '",width: "' + $scope.data.width + '",background_color: "' + $scope.data.background_color + '",header_color: "' + $scope.data.header_color + '",row_background_color:"' + $scope.data.row_background_color + '",text_color: "' + $scope.data.text_color + '",link_color:"' + $scope.data.text_color + '",max_number_of_events:"' + $scope.data.max_number_of_events + '",group_series_events:"' + $scope.data.group_series_events + '",display_title_bar:"' + $scope.data.display_title_bar + '",include_scrollbar: "auto", display_date_block: "' + $scope.data.display_date_block + '",display_image: "none",display_full_date: "' + $scope.data.display_full_date + '",display_time: "' + $scope.data.display_time + '",display_venue: "true", display_tickets_link: "none"}</script><script id="EventsWidgetScript" type="text/javascript" src="https://tickets.alistixs.com/js/events_widget.js"></script>';
+    }
+
 
 });
